fab8473414fa9ebdf30dd0e72166a4bda2c17021
Jeff Tian <jeff.tian@outlook.com>
2022-12-09 18:38:39 +0800

    [BV-27][cicd] add ci.sh and setup node in ci step

---
+++ .github/ci.sh
@@ -0,0 +1,15 @@
+cd brickstage
+npm i -g yarn
+yarn install --production=false
+yarn tsc
+yarn build:backend
+yarn build-image --tag jefftian/backstage
+docker images
+docker run --network host -e CI=true -d -p 127.0.0.1:7007:7007 --name backstage jefftian/backstage
+docker ps | grep -q backstage
+docker ps -aqf "name=backstage$"
+docker push jefftian/backstage
+docker logs $(docker ps -aqf name=backstage$)
+curl localhost:7007 || docker logs $(docker ps -aqf name=backstage$)
+docker kill backstage || echo "backstage killed"
+docker rm backstage || echo "backstage removed"
